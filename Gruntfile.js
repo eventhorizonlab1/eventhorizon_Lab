640f29c131243bba43b4be72e685d441246f699e
eventhorizonlab1 <eventhorizonlab1@gmail.com>
2025-11-26 10:49:13 +0100

    refactor: Remove Grunt and Gruntfile
    The Grunt build system and its configuration are no longer used and have been removed to streamline the project setup.
    
    Additionally, the video loading delay has been optimized in `VideoSection.tsx` for a smoother user experience.

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-  // Configuration vide pour l'instant
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  });
-
-  // Tâche par défaut qui ne fait rien mais dit que tout va bien
-  grunt.registerTask('default', []);
-};
